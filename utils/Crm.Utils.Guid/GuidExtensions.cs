6d34bfbca705b258a5a934e2ebbb94b9aae575ee
Usman Ayupov <au073@mail.ru>
2019-05-14 00:54:19 +0300

    Add guid extensions

---
+++ utils/Crm.Utils.Guid/GuidExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Crm.Utils.Guid
+{
+    public static class GuidExtensions
+    {
+        public static bool IsEmpty(this System.Guid? value)
+        {
+            return !value.HasValue || IsEmpty(value.Value);
+        }
+        
+        public static bool IsEmpty(this System.Guid value)
+        {
+            return value == System.Guid.Empty;
+        }
+    }
+}
