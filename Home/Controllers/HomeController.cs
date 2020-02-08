b0fa17eca7e4bdeb01e1a9e833858f7273066fbd
Usman Ayupov <u.ayupov@dodopizza.com>
2020-02-08 20:34:05 +0300

    Ignore API for     public class HomeController : DefaultMvcController

--- Home/Controllers/HomeController.cs
+++ Home/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using Ajupov.Infrastructure.All.Mvc;
+﻿using Ajupov.Infrastructure.All.ApiDocumentation.Attributes;
+using Ajupov.Infrastructure.All.Mvc;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Crm.Apps.Home.Controllers
 {
+    [IgnoreApiDocumentation]
     [Route("")]
     public class HomeController : DefaultMvcController
     {
